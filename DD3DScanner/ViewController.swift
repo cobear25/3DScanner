876cd43cc6be322bf13e49246e5b476ed8135476
Cody Mace <cody@structionsite.com>
2023-05-17 10:40:43 -0600

    Initial Commit

---
+++ DD3DScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DD3DScanner
+//
+//  Created by Cody Mace on 5/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
