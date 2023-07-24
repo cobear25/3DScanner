3651b4d9b88f6f67e5ad3a4fcfe5e22424ab4042
Cody Mace <cody@structionsite.com>
2023-07-23 19:59:47 -0600

    Add simple scanning and exporting.

--- DD3DScanner/ViewController.swift
+++ DD3DScanner/ViewController.swift
@@ -6,14 +6,135 @@
 //
 
 import UIKit
+import Metal
+import MetalKit
+import ARKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController, ARSessionDelegate {
+    @IBOutlet weak var scanButton: UIButton!
+    private let isUIEnabled = true
+    
+    private let session = ARSession()
+    private var renderer: Renderer!
+    var isScanning = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let device = MTLCreateSystemDefaultDevice() else {
+            print("Metal is not supported on this device")
+            return
+        }
+        
+        session.delegate = self
+        
+        // Set the view to use the default device
+        if let view = view as? MTKView {
+            view.device = device
+            
+            view.backgroundColor = UIColor.clear
+            // we need this to enable depth test
+            view.depthStencilPixelFormat = .depth32Float
+            view.contentScaleFactor = 1
+            view.delegate = self
+            
+            // Configure the renderer to draw to the view
+            renderer = Renderer(session: session, metalDevice: device, renderDestination: view)
+            renderer.drawRectResized(size: view.bounds.size)
+        }
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    // Create a world-tracking configuration, and
+        // enable the scene depth frame-semantic.
 
+    
+    @IBAction func scanOrExport(_ sender: Any) {
+        if !isScanning {
+            // Create a world-tracking configuration, and
+            // enable the scene depth frame-semantic.
+            let configuration = ARWorldTrackingConfiguration()
+            configuration.frameSemantics = .sceneDepth
+            // Run the view's session
+            session.run(configuration)
+            
+            // The screen shouldn't dim during AR experiences.
+            UIApplication.shared.isIdleTimerDisabled = true
+            isScanning = true
+            scanButton.setTitle("Export", for: .normal)
+        } else {
+            if let file = self.renderer.savePointsToFile()
+            {
+                var filesToShare = [Any]()
+                filesToShare.append(file)
+                let controller = UIActivityViewController(activityItems: filesToShare, applicationActivities: nil)
+                self.present(controller, animated: true)
+            }
+        }
+    }
+    
+    // Auto-hide the home indicator to maximize immersion in AR experiences.
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
+    }
+    
+    // Hide the status bar to maximize immersion in AR experiences.
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user.
+        guard error is ARError else { return }
+        let errorWithInfo = error as NSError
+        let messages = [
+            errorWithInfo.localizedDescription,
+            errorWithInfo.localizedFailureReason,
+            errorWithInfo.localizedRecoverySuggestion
+        ]
+        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
+        DispatchQueue.main.async {
+            // Present an alert informing about the error that has occurred.
+            let alertController = UIAlertController(title: "The AR session failed.", message: errorMessage, preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
+                alertController.dismiss(animated: true, completion: nil)
+                if let configuration = self.session.configuration {
+                    self.session.run(configuration, options: .resetSceneReconstruction)
+                }
+            }
+            alertController.addAction(restartAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
 
+// MARK: - MTKViewDelegate
+
+extension ViewController: MTKViewDelegate {
+    // Called whenever view changes orientation or layout is changed
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        renderer.drawRectResized(size: size)
+    }
+    
+    // Called whenever the view needs to render
+    func draw(in view: MTKView) {
+        renderer.draw()
+    }
+}
+
+// MARK: - RenderDestinationProvider
+
+protocol RenderDestinationProvider {
+    var currentRenderPassDescriptor: MTLRenderPassDescriptor? { get }
+    var currentDrawable: CAMetalDrawable? { get }
+    var colorPixelFormat: MTLPixelFormat { get set }
+    var depthStencilPixelFormat: MTLPixelFormat { get set }
+    var sampleCount: Int { get set }
+}
+
+extension MTKView: RenderDestinationProvider {
+    
 }
 
